e881e8c1fa119b3d0b07e69d28c9d099e655c433
goph-R <gopher.hu@gmail.com>
2019-12-12 00:30:34 +0100

    Initial commit.

---
+++ BulmaForm.php
@@ -0,0 +1,30 @@
+<?php
+
+class BulmaForm extends Form {
+
+    protected function getInputClassMap() {
+        return [
+            'TextInput' => 'input',
+            'PasswordInput' => 'input',
+            'TextareaInput' => 'textarea',
+            'CheckboxInput' => 'checkbox',
+            'SubmitInput' => 'button is-link'
+        ];
+    }
+    
+    public function fetch($path = ':form/form') {
+        $inputClassMap = $this->getInputClassMap();
+        foreach (array_values($this->inputs) as $input) {
+            $class = get_class($input);
+            if (isset($inputClassMap[$class])) {
+                $input->addClass($inputClassMap[$class]);
+            }
+            if ($input->hasError()) {
+                $input->addClass('is-danger');
+            }
+        }
+        return parent::fetch($path);
+    }    
+    
+    
+}
---
+++ BulmaModule.php
@@ -0,0 +1,32 @@
+<?php
+
+class BulmaModule extends Module {
+    
+    const CONFIG_FONTAWESOME_URL = 'bulma.fontawesome_url';
+    const DEFAULT_FONTAWESOME_URL = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css';
+    const CONFIG_URL = 'bulma.url';
+    const DEFAULT_URL = 'https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css';
+
+    /** @var Framework */
+    protected $framework;
+    protected $id = 'minicore-bulma';
+
+    public function __construct(Framework $framework) {
+        parent::__construct($framework);
+        $framework->changeClass('Form', 'BulmaForm');
+        $this->framework = $framework;
+    }
+    
+    public function init() {
+        /** @var Router $router */
+        $router = $this->framework->get('router');
+        /** @var View $view */
+        $view = $this->framework->get('view');
+        /** @var Config $config */
+        $config = $this->framework->get('config');
+        $view->addStyle($config->get(self::CONFIG_FONTAWESOME_URL, self::DEFAULT_FONTAWESOME_URL));
+        $view->addStyle($config->get(self::CONFIG_URL, self::DEFAULT_URL));
+        $view->addFolder(':form', 'modules/minicore-bulma/templates/form');
+    }
+
+}
